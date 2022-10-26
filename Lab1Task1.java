8282b4285dfa245455ce78142bb407b8ed32a960
Mohsinali123m <hafizmohsinali00123@gmail.com>
2022-10-25 21:23:48 -0700

    commiting

---
+++ Lab1Task1.java
@@ -0,0 +1,48 @@
+class Lab1Task1{
+    public static void main(String[] args) {
+
+	String city = "Hyd";
+	String province = "Sindh";
+	String country = "Pak";
+
+	int num1=10;
+	int num2=20;
+	int num3=30;
+
+	float pi=3.14f;
+	float e=2.718f;
+	float L=0.5f;
+	 
+	boolean isFirstProgram = true;
+	Boolean isThisJavaClass =true;
+	Boolean isLastProgramOfTask=false;
+
+	char CL='A';
+	char SL='a';
+	char flmn='M';
+
+        System.out.println("City: "+ city);
+        System.out.println("Province: "+ province);
+        System.out.println("Country: "+ country);
+	System.out.println();
+
+	System.out.println("num1: "+num1);
+	System.out.println("num2: "+num2);
+	System.out.println("num3: "+num3);
+	System.out.println();
+	
+	System.out.println("Pi: "+pi);
+	System.out.println("e: "+e);
+	System.out.println("L: "+L);
+	System.out.println();
+
+	System.out.println("Is first program? "+ isFirstProgram);
+	System.out.println("Is this java class? "+ isThisJavaClass);
+	System.out.println("Is last program of task? "+ isLastProgramOfTask);
+	System.out.println();
+
+	System.out.println("Capital Letter: "+ CL);
+	System.out.println("Small Letter: "+ SL);
+	System.out.println("The first letter of my name: "+ flmn);
+    }
+}
