c43376b2ec0047024591bffa56750be221ce77be
Mohsinali123m <hafizmohsinali00123@gmail.com>
2022-10-30 04:55:22 -0700

    arithmetic & logical Operators

---
+++ Lab1Task3.java
@@ -0,0 +1,38 @@
+class Lab1Task3{
+
+	public static void main(String[] args){
+
+		int num1 = 20;
+		int num2 = 10;
+
+		System.out.println("Num1 is " + num1);		
+		System.out.println("Num2 is " + num2);		
+		System.out.println();
+
+		System.out.println("Arithmetic operators:");
+
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2) ); // Addition		
+
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2)); //Subtraction		
+
+		System.out.println(num1 + " * " + num2 + " = " + (num1 * num2)); // Multilication		
+
+		System.out.println(num1 + " / " + num2 + " = " + (num1 / num2)); // Divition
+
+		System.out.println("Logical operators:");
+
+		System.out.println(num1 == num2);	// False
+
+		System.out.println(num1 != num2);	// True
+
+		System.out.println(num1 > num2);	// True
+
+		System.out.println(num1 < num2);	// False
+
+		System.out.println(num1 >= num2);	// True
+
+		System.out.println(num1 <= num2);	// False	
+
+	}	
+
+}
